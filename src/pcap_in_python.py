5d828b65e061b5a4c5bdf1d10e69d078567b4f50
Felix <fruessli@student.ethz.ch>
2019-11-24 13:47:01 +0000

    We can now read src+dst-IP/ -IPv6 /-MAC /-port from .pcap-files in python. Tested it with Ethernet_test_pcap.pcap.

--- src/pcap_in_python.py
+++ src/pcap_in_python.py
@@ -1,39 +1,42 @@
 from scapy.all import *
 
-# TODO: try Ether(src=get_if_hwaddr(interface)) somehow
-
 def main():
-    pcap_path = "first500.pcap"
+    pcap_path = "Ethernet_test_pcap.pcap"
     pkts = rdpcap(pcap_path)
 
-    mac_available = 0
     for pkt in pkts:
+        protocol = 0
+
         if IP in pkt:
             src_ip = pkt[IP].src
             dst_ip = pkt[IP].dst
             protocol = pkt[IP].proto
             src_port = pkt[IP].sport
             dst_port = pkt[IP].dport
-            # There seems to be no Ethernet traffic recorded in the CAIDA file
-            try:
-                # prints this PC's MAC
-                src_mac = Ether().src
-                dst_mac = Ether().dst
-                mac_available = 1
-            except:
-                pass
 
+            print("##### ")
             print("##### IP: from [{}] --> to [{}]".format(src_ip, dst_ip))
-            if mac_available == 1:
-                print("##### MAC: from [{}] --> to [{}]".format(src_mac, dst_mac))
             if protocol == 6:
-                # print("##### TCP")
                 print("##### TCP: from [{}] --> to [{}]".format(src_port, dst_port))
             elif protocol == 17:
-                # print("##### UDP")
                 print("##### UDP: from [{}] --> to [{}]".format(src_port, dst_port))
 
-    print("test : {}".format(pkt[IP].src))
+        elif IPv6 in pkt:
+            src_ip = pkt[IPv6].src
+            dst_ip = pkt[IPv6].dst
+            # IPv6 doens't have protocol
+            src_port = pkt[IPv6].sport
+            dst_port = pkt[IPv6].dport
+
+            print("##### ")
+            print("##### IP: from [{}] --> to [{}]".format(src_ip, dst_ip))
+            print("##### Ports: from [{}] --> to [{}]".format(src_port, dst_port))
+
+        if Ether in pkt:
+            src_mac = pkt[Ether].src
+            dst_mac = pkt[Ether].dst
+
+            print("##### MAC: from [{}] --> to [{}]".format(src_mac, dst_mac))
 
 
 if __name__ == '__main__':
