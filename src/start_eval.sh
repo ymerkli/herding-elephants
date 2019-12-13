31f932197e67b74e1b487643602d67513a4990cf
tbohren <p4@p4>
2019-12-13 10:17:10 +0000

    bla bla

--- src/start_eval.sh
+++ src/start_eval.sh
@@ -4,7 +4,7 @@ lxterminal -e 'sudo p4run --conf p4app/p4app_10_switches.json' &
 echo 'Starting mininet...'
 sleep 30
 echo 'Starting evaluation...'
-sudo python start_multiple_eval_runs.py --s 0.1 --e 0.2 --t 50 --r 5 --c ../evaluation/accuracy_epsilon/measurements_normal_100k.csv --p ../data/eval100k.pcap
+sudo python start_multiple_eval_runs.py --s 0.1 --e 0.2 --t 50 --r 11 --c ../evaluation/accuracy_epsilon/measurements_normal_100k.csv --p ../data/eval100k.pcap
 echo "Sampling prob: $1, epsilon: $2, global_thresh: $3, reporting_tresh: $4, csv: $5 pcap: $6"
 
 sleep 100
